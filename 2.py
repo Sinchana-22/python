13c80993de1bf7408f87a6cb8db26561106eb6a5
sinchana <sinchanapatil2023@gmail.com>
2026-02-18 19:35:21 +0530

    day3 learning about input and comments in python

---
+++ 2.py
@@ -0,0 +1,22 @@
+age= 10
+name = "sinchana"
+markes=98.5
+is_pass=True
+print(type(name))
+print(type(is_pass))
+##arithmetic operations
+a=15
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#swapping of two numbers
+temp = a
+a = b
+b = temp
+print("After swapping: a =", a, "b =", b)
---
+++ 3.py
@@ -0,0 +1,11 @@
+# single line comment
+ 
+'''this is multiline comment'''
+# taking input and converting str to int
+print("hii")
+name=input("Enter your name:")
+print ( "my name is",name )
+my_age=int(input("myage:"))
+ur_age =int(input("ur age:"))
+age_diff=abs(my_age - ur_age)
+print(f"age difference is ",age_diff)
